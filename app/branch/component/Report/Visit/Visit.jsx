e0f79c6a463abbcf0c1419e8e75d5be951a1a450
teamparmjeet <teamparmjeet@gmail.com>
2025-01-21 10:53:18 +0530

    branch update

--- app/branch/component/Report/Visit/Visit.jsx
+++ app/branch/component/Report/Visit/Visit.jsx
@@ -10,9 +10,17 @@ export default function UnderVisit() {
   const [loading, setLoading] = useState(true);
   const { data: session } = useSession();
   const [adminData, setAdminData] = useState(null);
-  const [courses, setCourses] = useState({}); // To store course ID to name mapping
+  const [courses, setCourses] = useState({});
+  const [filters, setFilters] = useState({
+    studentName: "",
+    mobileNumber: "",
+    course: "",
+    qualification: "",
+    address: "",
+    reference: "",
+    status: "",
+  });
 
-  // Fetch admin data based on session email
   useEffect(() => {
     const fetchAdminData = async () => {
       if (!session?.user?.email) return;
@@ -20,7 +28,7 @@ export default function UnderVisit() {
       setLoading(true);
       try {
         const response = await axios.get(`/api/admin/find-admin-byemail/${session.user.email}`);
-        setAdminData(response.data.branch); // Ensure response contains branch
+        setAdminData(response.data.branch);
       } catch (err) {
         console.error("Error fetching admin data:", err.message);
       } finally {
@@ -31,7 +39,6 @@ export default function UnderVisit() {
     fetchAdminData();
   }, [session]);
 
-  // Fetch queries based on admin data
   useEffect(() => {
     const fetchQueryData = async () => {
       if (!adminData) return;
@@ -50,13 +57,12 @@ export default function UnderVisit() {
     fetchQueryData();
   }, [adminData]);
 
-  // Fetch all courses and map IDs to names
   useEffect(() => {
     const fetchCourses = async () => {
       try {
         const response = await axios.get(`/api/course/fetchall/courses`);
         const courseMapping = response.data.fetch.reduce((acc, course) => {
-          acc[course._id] = course.course_name; // Assuming each course has _id and name
+          acc[course._id] = course.course_name;
           return acc;
         }, {});
         setCourses(courseMapping);
@@ -68,10 +74,28 @@ export default function UnderVisit() {
     fetchCourses();
   }, []);
 
+  const handleFilterChange = (e, key) => {
+    setFilters({ ...filters, [key]: e.target.value });
+  };
+
+  const filteredQueries = queries.filter((query) => {
+    const courseName = courses[query.courseInterest] || "Unknown Course";
+    return (
+      query.studentName.toLowerCase().includes(filters.studentName.toLowerCase()) &&
+      query.studentContact.phoneNumber.includes(filters.mobileNumber) &&
+      courseName.toLowerCase().includes(filters.course.toLowerCase()) &&
+      query.qualification.toLowerCase().includes(filters.qualification.toLowerCase()) &&
+      query.studentContact.address.toLowerCase().includes(filters.address.toLowerCase()) &&
+      query.referenceid.toLowerCase().includes(filters.reference.toLowerCase()) &&
+      (filters.status === "" ||
+        (filters.status === "Enroll" && query.addmission) ||
+        (filters.status === "Pending" && !query.addmission))
+    );
+  });
+
   return (
     <div className="container mx-auto p-5">
       <div className="flex flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 lg:space-x-6">
-        {/* Queries List */}
         <div className="w-full">
           <div className="mb-6">
             <div className="p-4">
@@ -84,44 +108,99 @@ export default function UnderVisit() {
                   <table className="min-w-full text-xs text-left text-gray-600 font-sans">
                     <thead className="bg-[#29234b] text-white uppercase">
                       <tr>
-                        <th className="px-6 py-4">Student Name</th>
-                        <th className="px-6 py-4">Mobile Number</th>
-                        <th className="px-6 py-4">Course</th>
-                        <th className="px-6 py-4">Qualification</th>
-                        <th className="px-6 py-4">Address</th>
-                        <th className="px-6 py-4">Reference</th>
-                        <th className="px-6 py-4">Status</th>
+                        <th className="px-6 py-4">#</th>
+                        <th className="px-6 py-4">
+                          <input
+                            type="text"
+                            placeholder="Student Name"
+                            className="w-full p-1 text-black"
+                            value={filters.studentName}
+                            onChange={(e) => handleFilterChange(e, "studentName")}
+                          />
+                        </th>
+                        <th className="px-6 py-4">
+                          <input
+                            type="text"
+                            placeholder="Mobile Number"
+                            className="w-full p-1 text-black"
+                            value={filters.mobileNumber}
+                            onChange={(e) => handleFilterChange(e, "mobileNumber")}
+                          />
+                        </th>
+                        <th className="px-6 py-4">
+                          <input
+                            type="text"
+                            placeholder="Course"
+                            className="w-full p-1 text-black"
+                            value={filters.course}
+                            onChange={(e) => handleFilterChange(e, "course")}
+                          />
+                        </th>
+                        <th className="px-6 py-4">
+                          <input
+                            type="text"
+                            placeholder="Qualification"
+                            className="w-full p-1 text-black"
+                            value={filters.qualification}
+                            onChange={(e) => handleFilterChange(e, "qualification")}
+                          />
+                        </th>
+                        <th className="px-6 py-4">
+                          <input
+                            type="text"
+                            placeholder="Address"
+                            className="w-full p-1 text-black"
+                            value={filters.address}
+                            onChange={(e) => handleFilterChange(e, "address")}
+                          />
+                        </th>
+                        <th className="px-6 py-4">
+                          <input
+                            type="text"
+                            placeholder="Reference"
+                            className="w-full p-1 text-black"
+                            value={filters.reference}
+                            onChange={(e) => handleFilterChange(e, "reference")}
+                          />
+                        </th>
+                        <th className="px-6 py-4">
+                          <select
+                            className="w-full p-1 text-black"
+                            value={filters.status}
+                            onChange={(e) => handleFilterChange(e, "status")}
+                          >
+                            <option value="">All</option>
+                            <option value="Enroll">Enroll</option>
+                            <option value="Pending">Pending</option>
+                          </select>
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
-                      {queries.length > 0 ? (
-                        queries
-                          .sort((a, b) => {
-                            const gradeOrder = { A: 1, B: 2, C: 3, D: 4, F: 5 };
-                            return gradeOrder[a.grade] - gradeOrder[b.grade];
-                          })
-                          .map((query) => {
-                            const courseName = courses[query.courseInterest] || "Unknown Course";
-                            return (
-                              <tr
-                                key={query._id}
-                                className="border-b cursor-pointer transition-colors duration-200 hover:opacity-90"
-                              >
-                                <td className="px-6 py-1 font-semibold">
-                                  <Link href={`/branch/page/allquery/${query._id}`}>{query.studentName}</Link>
-                                </td>
-                                <td className="px-6 py-1">{query.studentContact.phoneNumber}</td>
-                                <td className="px-6 py-1">{courseName}</td>
-                                <td className="px-6 py-1">{query.qualification}</td>
-                                <td className="px-6 py-1">{query.studentContact.address}</td>
-                                <td className="px-6 py-1">{query.referenceid}</td>
-                                <td className="px-6 py-1">{query.addmission ? "Enroll" : "Pending"}</td>
-                              </tr>
-                            );
-                          })
+                      {filteredQueries.length > 0 ? (
+                        filteredQueries.map((query, index) => {
+                          const courseName = courses[query.courseInterest] || "Unknown Course";
+                          return (
+                            <tr
+                              key={query._id}
+                              className="border-b cursor-pointer transition-colors duration-200 hover:opacity-90"
+                            >
+                              <td className="px-6 py-1 font-semibold">{index + 1}</td>
+                              <td className="px-6 py-1 font-semibold">
+                                <Link href={`/branch/page/allquery/${query._id}`}>{query.studentName}</Link>
+                              </td>
+                              <td className="px-6 py-1">{query.studentContact.phoneNumber}</td>
+                              <td className="px-6 py-1">{courseName}</td>
+                              <td className="px-6 py-1">{query.qualification}</td>
+                              <td className="px-6 py-1">{query.studentContact.address}</td>
+                              <td className="px-6 py-1">{query.referenceid}</td>
+                              <td className="px-6 py-1">{query.addmission ? "Enroll" : "Pending"}</td>
+                            </tr>
+                          );
+                        })
                       ) : (
                         <tr>
-                          <td colSpan="7" className="px-6 py-4 text-center text-gray-500">
+                          <td colSpan="8" className="px-6 py-4 text-center text-gray-500">
                             No queries available
                           </td>
                         </tr>
