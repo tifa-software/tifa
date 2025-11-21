243a4ffcaa1cc353be451cbf18475e126b327802
teamparmjeet <teamparmjeet@gmail.com>
2025-11-21 15:19:40 +0530

    s

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -4,8 +4,8 @@ export const runtime = "nodejs";
 
 export async function GET(req) {
   // Authorization check (Required by Vercel Cron)
-  if (req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  if (req.headers.get("authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json({ error: "Unauthorized Tifa" }, { status: 401 });
   }
 
   // ⭐ Your scheduled task logic here
